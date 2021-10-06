e324023fcae957dd64f5e4e5dba0c9e61af5ce0f
zxcq544 <zxcq544@yandex.ru>
2021-10-06 17:49:51 +0300

    full big json

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,7 @@
 </script>
 
 <div class="products-catalog__list">
-	<Product json_data={$big_json[0]} />
-	<Product json_data={$big_json[1]} />
-	<Product json_data={$big_json[2]} />
-	<Product json_data={$big_json[3]} />
-	<Product json_data={$big_json[4]} />
-
-	<Product json_data={$big_json[5]} />
-	<Product json_data={$big_json[6]} />
-	<Product json_data={$big_json[7]} />
-	<Product json_data={$big_json[8]} />
-	<Product json_data={$big_json[9]} />
+	{#each $big_json as json_data}
+		<Product {json_data} />
+	{/each}
 </div>
